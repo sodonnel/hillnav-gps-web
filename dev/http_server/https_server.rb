1b985b3540450b2a8ed9a8c8c230790b875ae6d2
S O'Donnell <sodonnell@cloudera.com>
2024-01-29 22:30:27 +0000

    Allow dev server to run from a build directory

--- dev/http_server/https_server.rb
+++ dev/http_server/https_server.rb
@@ -11,12 +11,30 @@ require 'openssl'
 cert = OpenSSL::X509::Certificate.new File.read './cert.pem'
 pkey = OpenSSL::PKey::RSA.new File.read './key.pem'
 
+doc_root = File.join(__dir__, '..', '..')
+
+# Serve a specific build
+if ARGV.length > 0
+  doc_root = File.join(doc_root, ARGV[0])
+end
+puts "Serving #{doc_root}"
+
 server = WEBrick::HTTPServer.new(:Port => 8000,
                                  :SSLEnable => true,
                                  :SSLCertificate => cert,
                                  :SSLPrivateKey => pkey,
-                                 :DocumentRoot => "#{__dir__}/../..")
+                                 :DocumentRoot => doc_root)
 
-trap 'INT' do server.shutdown end
+server_insecure = WEBrick::HTTPServer.new(:Port => 8001,
+                                          :SSLEnable => false,
+                                          :DocumentRoot => doc_root)
 
-server.start
+trap 'INT' do
+  server.shutdown
+  server_insecure.shutdown
+end
+
+[
+  Thread.new { server.start },
+  Thread.new { server_insecure.start }
+].each(&:join)
